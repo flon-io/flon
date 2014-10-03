1b411cc3999c6e9c762ab0a3a03a7135b0315be4
John Mettraux <jmettraux@gmail.com>
2014-10-03 11:26:06 +0900

    scaffold invoker spec

---
+++ spec/i_invoker_spec.c
@@ -0,0 +1,18 @@
+
+//
+// specifying flon-invoker
+//
+// Fri Oct  3 11:24:25 JST 2014
+//
+
+#include "fl_invoker.h"
+
+
+context "flon-invoker"
+{
+  describe "flon_invoke_x()"
+  {
+    it "flips burgers"
+  }
+}
+
