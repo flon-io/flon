eea12252732a625e9ab5d593619ff1d7b29f2d1c
John Mettraux <jmettraux@gmail.com>
2014-10-06 16:21:46 +0900

    scaffold dispatcher specs

---
+++ spec/d_dispatcher_spec.c
@@ -0,0 +1,18 @@
+
+//
+// specifying flon-dispatcher
+//
+// Mon Oct  6 16:13:47 JST 2014
+//
+
+#include "fl_dispatcher.h"
+
+
+context "flon-dispatcher"
+{
+  describe "flon_dispatch_j()"
+  {
+    it "dispatches invocations"
+  }
+}
+
