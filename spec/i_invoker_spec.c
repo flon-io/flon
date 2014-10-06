8b5191dc0152428884742f67380f486ba5db5cb2
John Mettraux <jmettraux@gmail.com>
2014-10-06 10:35:38 +0900

    add expectations to the invoker spec

--- spec/i_invoker_spec.c
+++ spec/i_invoker_spec.c
@@ -5,6 +5,7 @@
 // Fri Oct  3 11:24:25 JST 2014
 //
 
+#include "flutil.h"
 #include "fl_common.h"
 #include "fl_invoker.h"
 
@@ -31,6 +32,16 @@ context "flon-invoker"
           "hello: world\n"
         "}\n"
       ));
+
+      sleep(1);
+
+      char *s = flu_readall("../tst/var/spool/in/inv_123456_ret.json");
+      //printf(">>>\n%s<<<\n", s);
+      expect(strstr(s, ",\"stamp\":\"") != NULL);
+
+      s = flu_readall("../tst/var/log/invocations/123456.txt");
+      //printf(">>>\n%s<<<\n", s);
+      expect(strstr(s, " stamp.rb over.") != NULL);
     }
   }
 }
