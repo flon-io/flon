a55f95cd439e979a396dc44c8749411bde1a65db
John Mettraux <jmettraux@gmail.com>
2014-10-23 14:03:23 +0900

    rework dispatcher spec

--- spec/d_dispatcher_spec.c
+++ spec/d_dispatcher_spec.c
@@ -24,14 +24,16 @@ context "flon-dispatcher"
     chdir("../tst");
     flon_configure(".");
 
-    char *id = NULL;
+    char *exid = NULL;
+    char *nid = NULL;
     char *name = NULL;
     char *path = NULL;
     char *s = NULL;
   }
   after each
   {
-    if (id) free(id);
+    if (exid) free(exid);
+    if (nid) free(nid);
     if (name) free(name);
     if (path) free(path);
     if (s) free(s);
@@ -41,20 +43,19 @@ context "flon-dispatcher"
   {
     it "dispatches invocations"
     {
-      id = flon_generate_exid("dtest.inv");
-      name = flu_sprintf("inv_%s.json", id);
-      path = flu_sprintf("var/spool/dis/%s", name);
+      exid = flon_generate_exid("dtest.inv");
+      name = flu_sprintf("inv_%s-0_2.json", exid);
 
       int r = flu_writeall(
-        path,
+        "var/spool/dis/inv_%s-0_2.json", exid,
         "{"
           "invoke: [ stamp, {}, [] ]\n"
-          "id: %s\n"
+          "exid: %s\n"
+          "nid: 0_2\n"
           "payload: {\n"
             "hello: world\n"
           "}\n"
-        "}",
-        id
+        "}", exid
       );
       expect(r == 1);
 
@@ -63,28 +64,29 @@ context "flon-dispatcher"
 
       sleep(1);
 
-      s = flu_readall("var/spool/dis/ret_%s.json", id);
-      //printf(">>>\n%s<<<\n", s);
-      expect(s != NULL);
-      expect(strstr(s, ",\"stamp\":\"") != NULL);
-      free(s);
+      fdja_value *v = fdja_parse_f("var/spool/dis/ret_%s-0_2.json", exid);
+      expect(v != NULL);
+      puts(fdja_to_pretty_djan(v));
+      expect(fdja_l(v, "stamp", NULL) != NULL);
+      fdja_free(v);
 
-      s = flu_readall("var/log/inv/inv_%s.txt", id);
+      s = flu_readall("var/log/inv/inv_%s-0_2.txt", exid);
       //printf(">>>\n%s<<<\n", s);
       expect(s != NULL);
       expect(strstr(s, " invoked >ruby stamp.rb<") != NULL);
       expect(strstr(s, " stamp.rb over.") != NULL);
 
-      expect(flu_fstat("var/spool/dis_%s.json", id) == 0);
+      expect(flu_fstat("var/spool/dis/%s", name) == 0);
 
-      flu_unlink("var/spool/dis/inv_%s_ret.json", id);
-      flu_unlink("var/log/inv/%s.txt", id);
+      flu_unlink("var/spool/dis/ret_%s-0_2.json", exid);
+      flu_unlink("var/spool/inv/inv_%s-0_2.json", exid);
+      flu_unlink("var/log/inv/inv_%s-0_2.txt", exid);
     }
 
     it "rejects files it doesn't understand"
     {
-      id = flon_generate_exid("dtest.rju");
-      name = flu_sprintf("inv_%s.json", id);
+      exid = flon_generate_exid("dtest.rju");
+      name = flu_sprintf("inv_%s.json", exid);
       path = flu_sprintf("var/spool/dis/%s", name);
 
       int r = flu_writeall(path, "NADA");
@@ -93,16 +95,16 @@ context "flon-dispatcher"
       r = flon_dispatch(name);
       expect(r == 1);
 
-      s = flu_readall("var/spool/rejected/inv_%s.json", id);
+      s = flu_readall("var/spool/rejected/inv_%s.json", exid);
       expect(s === "NADA");
 
-      flu_unlink("var/spool/rejected/inv_%s.json", id);
+      flu_unlink("var/spool/rejected/inv_%s.json", exid);
     }
 
     it "rejects files it doesn't know how to dispatch"
     {
-      id = flon_generate_exid("dtest.rjk");
-      name = flu_sprintf("inv_%s.json", id);
+      exid = flon_generate_exid("dtest.rjk");
+      name = flu_sprintf("inv_%s.json", exid);
       path = flu_sprintf("var/spool/dis/%s", name);
 
       int r = flu_writeall(
@@ -113,18 +115,17 @@ context "flon-dispatcher"
           "payload: {\n"
             "hello: world\n"
           "}\n"
-        "}",
-        id
+        "}", exid
       );
       expect(r == 1);
 
       r = flon_dispatch(name);
       expect(r i== 1);
 
-      s = flu_readall("var/spool/rejected/inv_%s.json", id);
+      s = flu_readall("var/spool/rejected/inv_%s.json", exid);
       expect(s ^== "{nada: [ stamp");
 
-      flu_unlink("var/spool/rejected/inv_%s.json", id);
+      flu_unlink("var/spool/rejected/inv_%s.json", exid);
     }
   }
 }
