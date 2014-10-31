210620ec14b49c1914960864408dee6c393f259b
John Mettraux <jmettraux@gmail.com>
2014-11-01 05:57:31 +0900

    let sgmail read from sendgrid.credentials

--- tst/usr/local/inv/sgmail/src/sgmail.c
+++ tst/usr/local/inv/sgmail/src/sgmail.c
@@ -26,6 +26,8 @@
 #define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 //#include "flutil.h"
 #include "bocla.h"
@@ -33,20 +35,76 @@
 
 int main(int argc, char *argv[])
 {
-  char *s =
-    "api_user=your_sendgrid_username&"
-    "api_key=your_sendgrid_password&"
-    "to=jmettraux+flon@gmail.com&"
-    "toname=JohnFlon&"
-    "subject=test0&"
-    "text=loreipsumtralala&"
-    "from=jmettraux+flon_server@gmail.com";
+  // TODO: read payload
+
+  char *u = flu_readall("sendgrid.credentials");
+  if (u == NULL)
+  {
+    puts("{\"outcome\":\"couldn't send: couldn't read sengrid.credentials\"}");
+    return 1;
+  }
+  *(strchr(u, '\n')) = '\0'; // trim right
+  char *col = strchr(u, ':');
+  if (col == NULL)
+  {
+    puts("{\"outcome\":\"couldn't send: couldn't split sengrid.credentials\"}");
+    return 1;
+  }
+  *col = '\0';
+  char *p = col + 1;
+
+  char *s = NULL;
+  flu_sbuffer *sb = flu_sbuffer_malloc();
+
+  s = flu_urlencode(u, -1);
+  flu_sbprintf(sb, "api_user=%s&", s);
+  free(s);
+
+  s = flu_urlencode(p, -1);
+  flu_sbprintf(sb, "api_key=%s&", s);
+  free(s);
+
+  free(u);
+
+  s = flu_urlencode("jmettraux+flon@gmail.com", -1);
+  flu_sbprintf(sb, "to=%s&", s);
+  free(s);
+
+  s = flu_urlencode("John Mettraux (flon)", -1);
+  flu_sbprintf(sb, "toname=%s&", s);
+  free(s);
+
+  s = flu_urlencode("maitre corbeau sur un arbe perche", -1);
+  flu_sbprintf(sb, "subject=%s&", s);
+  free(s);
+
+  s = flu_urlencode(
+    "Maitre corbeau, sur un arbre perche,\n"
+    "Tenait en son bec un fromage.\n"
+    "Maitre renard, par l'odeur alleche,\n"
+    "Lui tint a peu pres ce language :\n"
+    "...\n",
+    -1);
+  flu_sbprintf(sb, "text=%s&", s);
+  free(s);
+
+  s = flu_urlencode("jmettraux+flon_sgmail@gmail.com", -1);
+  flu_sbprintf(sb, "from=%s", s);
+  free(s);
+
+  char *form = flu_sbuffer_to_string(sb);
 
   fcla_response *res = fcla_post(
-    "https://api.sendgrid.com/api/mail.send.json", NULL, s);
+    "https://api.sendgrid.com/api/mail.send.json", NULL, form);
+
+  free(form);
 
   printf("res: %i\n", res->status_code);
+  printf("body: >%s<\n", res->body);
 
+  fcla_response_free(res);
+
+  puts("{\"outcome\":\"ok\"}");
   return 0;
 }
 
