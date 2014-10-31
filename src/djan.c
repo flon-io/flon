16dfa7ec41211f31b265389c4e704b9748186289
John Mettraux <jmettraux@gmail.com>
2014-10-31 13:04:02 +0900

    upgrade djan (fix symbol/number confusion)

--- src/djan.c
+++ src/djan.c
@@ -776,7 +776,8 @@ void fdja_to_d(FILE *f, fdja_value *v, int flags, size_t depth)
 
   // actual value
 
-  if (v->type == 'q' || v->type == 's')
+  //if (v->type == 'q' || v->type == 's')
+  if (v->type == 'q' || v->type == 's' || v->type == 'y')
   {
     fputs(stringc, f);
 
@@ -789,11 +790,11 @@ void fdja_to_d(FILE *f, fdja_value *v, int flags, size_t depth)
 
     fputs(clearc, f);
   }
-  else if (v->type == 'y')
-  {
-    s = fdja_string(v);
-    fprintf(f, "%s%s%s", stringc, s, clearc);
-  }
+  //else if (v->type == 'y')
+  //{
+  //  s = fdja_string(v);
+  //  fprintf(f, "%s%s%s", stringc, s, clearc);
+  //}
   else if (v->type == 'a' || v->type == 'o')
   {
     if (ol)
