1e33841f8bbe132762874ae7745cf67d108f8f96
John Mettraux <jmettraux@gmail.com>
2014-11-01 13:56:09 +0900

    add one more step to the rgb launch.rb execution

--- bin/launch.rb
+++ bin/launch.rb
@@ -8,9 +8,10 @@ f = File.open("tst/var/spool/dis/exe_#{id}.json", 'w')
 f.write(%[
 execute:
   [ sequence, {}, [
-    #[ invoke, { _0: stamp, color: blue }, [] ]
-    #[ invoke, { _0: stamp, color: green }, [] ]
-    [ invoke, { _0: sgmail, subjet: lila }, [] ]
+    [ invoke, { _0: stamp, color: blue }, [] ]
+    [ invoke, { _0: stamp, color: green }, [] ]
+    [ invoke, { _0: stamp, color: red }, [] ]
+    #[ invoke, { _0: sgmail, subjet: lila }, [] ]
   ] ]
 exid: #{id}
 payload: {
