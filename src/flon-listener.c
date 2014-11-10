f759490879e006067582dc013a1dfaf8d39102a2
John Mettraux <jmettraux@gmail.com>
2014-11-10 16:05:48 +0900

    link to /i/in and /i/executions/...

--- src/flon-listener.c
+++ src/flon-listener.c
@@ -32,21 +32,56 @@
 
 #include "flutil.h"
 #include "gajeta.h"
+#include "djan.h"
 #include "shervin.h"
 #include "shv_protected.h"
 #include "fl_common.h"
 
 
-static int i_handler(shv_request *req, shv_response *res, flu_dict *params)
+static int respond(shv_response *res, fdja_value *val)
 {
   res->status_code = 200;
 
   flu_list_set(
     res->headers, "content-type", strdup("application/json; charset=UTF-8"));
 
-  flu_list_add(res->body, strdup("{ \"nada\": 1 }"));
+  flu_list_add(res->body, fdja_to_json(val));
 
   return 1;
+}
+
+static int i_handler(shv_request *req, shv_response *res, flu_dict *params)
+{
+  fdja_value *r = fdja_v("{ _links: {} }");
+  fdja_value *l = fdja_l(r, "_links");
+
+  fdja_set(
+    l, "self",
+    fdja_v("{ href: \"%s\" }", shv_abs(0, req->uri_d)));
+
+  fdja_set(
+    l, "http://flon.io/rels.html#in",
+    fdja_v(
+      "{ href: \"%s\", method: POST }",
+      shv_rel(0, req->uri_d, "in")));
+
+  fdja_set(
+    l, "http://flon.io/rels.html#executions",
+    fdja_v(
+      "{ href: \"%s\", templated: true }",
+      shv_rel(0, req->uri_d, "executions")));
+  fdja_set(
+    l, "http://flon.io/rels.html#domain-executions",
+    fdja_v(
+      "{ href: \"%s\", templated: true }",
+      shv_rel(0, req->uri_d, "executions/{domain}")));
+  fdja_set(
+    l, "http://flon.io/rels.html#execution",
+    fdja_v(
+      "{ href: \"%s\", templated: true }",
+      shv_rel(0, req->uri_d, "executions/{exid}")));
+
+  return respond(res, r);
 }
 
 static void print_usage()
