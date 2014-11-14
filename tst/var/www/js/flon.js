cedd5f0c532b8590deec11cb965bba15c03abe04
John Mettraux <jmettraux@gmail.com>
2014-11-14 11:31:25 +0900

    bring handlebars in

--- tst/var/www/js/flon.js
+++ tst/var/www/js/flon.js
@@ -15,3 +15,10 @@ function byclass(cl)
   return a;
 }
 
+Date.prototype.to_s = function()
+{
+  return '' +
+    this.getFullYear() + '-' + this.getMonth() + '-' + this.getDate() + ' ' +
+    this.getHours() + ':' + this.getMinutes() + ':' + this.getSeconds();
+}
+
