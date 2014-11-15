086c46459bcd5c28c24e2d44b5b07b86bfe65924
John Mettraux <jmettraux@gmail.com>
2014-11-15 18:03:07 +0900

    scaffold null invoker (mostly its spec)

---
+++ spec/k/null_spec.c
@@ -0,0 +1,72 @@
+
+//
+// specifying flon invokers
+//
+// Sat Nov 15 17:50:46 JST 2014
+//
+
+#include "gajeta.h"
+#include "fl_ids.h"
+#include "fl_common.h"
+#include "fl_invoker.h"
+
+
+context "invoker: null"
+{
+  before each
+  {
+    fgaj_conf_get()->logger = fgaj_grey_logger;
+    fgaj_conf_get()->level = 5;
+    fgaj_conf_get()->out = stderr;
+    fgaj_conf_get()->params = "5p";
+
+    chdir("../tst");
+    flon_configure(".");
+
+    char *exid = NULL;
+    char *nid = NULL;
+    char *path = NULL;
+  }
+  after each
+  {
+    free(exid);
+    //free(nid); // no, it's not on the heap
+    free(path);
+  }
+
+  describe "null"
+  {
+    it "swallows any workitem"
+    {
+      exid = flon_generate_exid("ktest-null-0");
+      nid = "0_1";
+      path = flu_sprintf("var/spool/inv/inv_%s-%s.json", exid, nid);
+
+      flu_writeall(
+        path,
+        "invoke: [ null, {}, [] ]\n"
+        "exid: %s\n"
+        "nid: %s\n"
+        "payload: {\n"
+          "hello: world\n"
+        "}\n",
+        exid, nid
+      );
+
+      int r = flon_invoke(path);
+
+      expect(r == 0);
+
+      sleep(1);
+
+      expect(flu_canopath(".") $==f "/tst/");
+
+      expect(flu_fstat("var/spool/inv/inv_%s-%s.json", exid, nid) == 0);
+        // the null participant nuked it
+
+      expect(flu_fstat("var/spool/dis/ret_%s-%s.json", exid, nid) c== 0);
+        // the null participant nuked it
+    }
+  }
+}
+
