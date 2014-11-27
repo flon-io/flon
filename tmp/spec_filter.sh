fe14743056bfc88b4f1eefc2b97657f431a4a910
John Mettraux <jmettraux@gmail.com>
2014-11-27 14:51:15 +0900

    introduce tmp/spec_filter.sh (for "make [v]spec")

---
+++ tmp/spec_filter.sh
@@ -0,0 +1,24 @@
+
+#
+# spec_filter.sh
+#
+# Thu Nov 27 14:32:12 JST 2014
+#
+# outputs success lines in green
+# and failure lines in red
+#
+
+while read LINE; do
+
+  # green
+  echo $LINE | GREP_COLORS="mt=01;32" grep -E " 0 failures" || :
+
+  # yellow
+  echo $LINE | GREP_COLORS="mt=01;33" grep -E " [0-9]+ pending" || :
+
+  # red
+  echo $LINE | grep -E "[1-9][0-9]* failures" || :
+  echo $LINE | grep -E "^make spec L=[0-9]+" || :
+  echo $LINE | grep -E "definitely lost: [1-9][0-9]* bytes" || :
+done
+
