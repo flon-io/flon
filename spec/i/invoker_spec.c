8ea0778885a15b2e26e31bbb4eac3dd6c237193b
John Mettraux <jmettraux@gmail.com>
2015-01-30 14:52:50 +0900

    use hlp_wait_for_file() in spec/i/invoker_spec.c

--- spec/i/invoker_spec.c
+++ spec/i/invoker_spec.c
@@ -12,6 +12,8 @@
 #include "fl_common.h"
 #include "fl_invoker.h"
 
+#include "flon_helpers.h"
+
 
 context "flon-invoker"
 {
@@ -50,14 +52,15 @@ context "flon-invoker"
 
       expect(r == 0);
 
-      sleep(1);
+      r = hlp_wait_for_file('f', "var/spool/inv/inv_%s-%s.json", exid, nid, 3);
+      expect(r i== 1);
 
       expect(flu_canopath(".") $==f "/tst/");
 
-      expect(flu_fstat("var/spool/inv/inv_%s-%s.json", exid, nid) == 'f');
+      expect(flu_fstat("var/spool/inv/inv_%s-%s.json", exid, nid) c== 'f');
         // it's still here, it's the dispatcher's work to nuke it
 
-      expect(flu_fstat("var/spool/dis/ret_%s-%s.json", exid, nid) == 'f');
+      expect(flu_fstat("var/spool/dis/ret_%s-%s.json", exid, nid) c== 'f');
 
       fdja_value *v = fdja_parse_f("var/spool/dis/ret_%s-%s.json", exid, nid);
       //puts(fdja_to_pretty_djan(v));
